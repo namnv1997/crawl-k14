b87c708ea4f8c988a1ced8fddce65ede28247c06
namnv1110 <helloimnvn@gmail.com>
2019-07-11 14:48:21 +0700

    edit push s3

--- venv/src/crawl.py
+++ venv/src/crawl.py
@@ -2,8 +2,9 @@ import json
 import codecs
 import requests
 import bs4
+import boto3
 import arrow
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 import output
 
 base_url = "http://kenh14.vn/"
@@ -12,7 +13,7 @@ def crawl_data_by_categories(list_categories):
     for category in list_categories:
         sub_link = "http://kenh14.vn/" + category + "/{}.chn"
         start_day = date(2019, 7, 1)
-        end_day = date(2019, 7, 3)
+        end_day = date(2019, 7, 2)
         count_day = 1
         numbers_day = end_day - start_day
         while end_day != start_day:
@@ -33,15 +34,18 @@ def get_links_by_day(link_day, category_name):
         src = bs4.BeautifulSoup(req.content, 'lxml')
         links = src.select('h3.knswli-title > a')
         count = 0
+        result = ""
         for a in links:
             article = get_article_content(base_url + a.attrs['href'][1:])
-            f = open('../output/'+category_name+'.txt', encoding='utf8', mode='a+')
-            f.write(str(article['content']))
-            f.write('\n')
+            # f = open('../output/'+category_name+'.txt', encoding='utf8', mode='a+')
+            # f.write(str(article['content']))
+            # f.write('\n')
             count += 1
+            result += article['content'] + "\n"
             print(str(round(count*100/len(links), 2)) + "%")
     else:
         print('Not Success')
+    update_object_s3(category_name, 'sia-03-project-data-source-2', result)
 
 def get_article_content(url):
     data = {}
@@ -62,16 +66,15 @@ def get_article_content(url):
         # data['pub_date'] = pub_date.text.strip() if pub_date else ''
     return data
 
-def update_object_s3(list_categories, bucketName):
+def update_object_s3(category_name, bucketName, content):
     s3 = boto3.client('s3')
-    for cate in list_categories:
-        file_name_computer = '../output/' + cate + '.txt'
-        file_name_aws = base_url + cate + '.txt'
-        try:
-            response = s3.upload_file(file_name_computer, bucketName, file_name_aws)
-        except Exception:
-            return None
-    return response
+    time_up = datetime.today().strftime("%m-%d-%Y")
+    file_name_aws = "kenh14/" + time_up + '/' + category_name + '.txt'
+    try:
+        # response = s3.put_object(file_name_computer, bucketName, file_name_aws)
+        s3.put_object(Body=content, Bucket=bucketName, Key=file_name_aws)
+    except Exception:
+        return None
 list_categories = ['star', 'tv-show', 'cine', 'musik', 'beauty-fashion', 'doi-song', 'an-ca-the-gioi', 'xa-hoi', 'the-gioi', 'sport', 'hoc-duong']
 crawl_data_by_categories(list_categories)
-update_object_s3(list_categories, 'sia-03-project-data-source-2')
+
